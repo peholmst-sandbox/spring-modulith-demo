caee1683c9b81dbf0d1069833046b67cb01024bc
Petter Holmstrom <petter@vaadin.com>
2025-02-26 09:05:53 +0200

    Change how threading is handled in Invoice Processing.

--- src/main/java/com/example/application/invoiceprocessing/InvoiceProcessing.java
+++ src/main/java/com/example/application/invoiceprocessing/InvoiceProcessing.java
@@ -8,6 +8,7 @@ import com.example.application.orderprocessing.service.OrderCreatedEvent;
 import com.example.application.paymentprocessing.service.PaymentReceivedEvent;
 import com.example.application.sharedkernel.domain.InterestRate;
 import com.example.application.sharedkernel.domain.Money;
+import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
@@ -17,10 +18,15 @@ import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.modulith.moments.DayHasPassed;
 import org.springframework.modulith.moments.support.Moments;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Component
 class InvoiceProcessing implements InvoiceService {
@@ -33,11 +39,24 @@ class InvoiceProcessing implements InvoiceService {
     private final Moments moments;
     private final ApplicationEventPublisher eventPublisher;
     private final InvoiceRepository invoiceRepository;
+    private final ExecutorService executorService;
+    private final TransactionTemplate transactionTemplate;
 
-    InvoiceProcessing(Moments moments, ApplicationEventPublisher eventPublisher, InvoiceRepository invoiceRepository) {
+    InvoiceProcessing(Moments moments, ApplicationEventPublisher eventPublisher, InvoiceRepository invoiceRepository, PlatformTransactionManager transactionManager) {
         this.moments = moments;
         this.eventPublisher = eventPublisher;
         this.invoiceRepository = invoiceRepository;
+        transactionTemplate = new TransactionTemplate(transactionManager);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        log.info("Starting executor service");
+        executorService = Executors.newSingleThreadExecutor();
+    }
+
+    @PreDestroy
+    void destroy() throws InterruptedException {
+        log.info("Shutting down executor service");
+        executorService.shutdown();
+        executorService.awaitTermination(30, TimeUnit.SECONDS);
     }
 
     @ApplicationModuleListener
@@ -66,18 +85,27 @@ class InvoiceProcessing implements InvoiceService {
     }
 
     @EventListener
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     void on(DayHasPassed event) {
-        log.info("Checking for overdue invoices on: {}", event.getDate());
-        invoiceRepository.findOverdue(event.getDate())
-                .filter(invoice -> !invoice.isPaid())
-                .forEach(invoice -> {
-                    invoice.addLateFee();
-                    invoiceRepository.save(invoice);
-                    var dto = createDto(invoice);
-                    log.info("Late fee added to invoice: {}", dto);
-                    eventPublisher.publishEvent(new InvoiceOverdueEvent(dto));
-                });
+        // This method uses its own executor service instead of @Async because of the time machine. If we jump ahead one
+        // week, a lot of events will be fired at the same time. When using @Async, they may start executing at the
+        // same time, resulting in some events being processed in the wrong order. By limiting the executor service to
+        // one thread and a queue, we can be sure the events are always processed in the same order.
+        //
+        // Because we use programmatic event execution, we have to use programmatic transaction management as well,
+        // since transactions are scoped to the current thread.
+        executorService.execute(() -> {
+            log.info("Checking for overdue invoices on: {}", event.getDate());
+            transactionTemplate.executeWithoutResult(tx -> invoiceRepository.findOverdue(event.getDate())
+                    .filter(invoice -> !invoice.isPaid())
+                    .forEach(invoice -> {
+                        invoice.addLateFee();
+                        invoiceRepository.save(invoice);
+                        var dto = createDto(invoice);
+                        log.info("Late fee added to invoice: {}", dto);
+                        eventPublisher.publishEvent(new InvoiceOverdueEvent(dto));
+                    })
+            );
+        });
     }
 
     @Override
